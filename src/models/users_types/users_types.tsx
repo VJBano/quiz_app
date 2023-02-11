c5a866b44620fb35d0985ef4b3206298d61ea264
Vincent Jay <vincentjaybano45@gmail.com>
2023-02-12 07:58:13 +0800

    test-firebase

---
+++ src/models/users_types/users_types.tsx
@@ -0,0 +1,7 @@
+export interface UserList {
+    id:number;
+    username:string;
+    email:string;
+    password:string;
+    recoveryKey:string;
+}
--- src/pages/login_page.tsx
+++ src/pages/login_page.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { SyntheticEvent } from 'react'
+import { Add_Sample, } from '../firebase/add_sample'
+import { UserList } from '../models/users_types/users_types'
 
 const Login_Page = () => {
+
+  const sample:UserList = ({
+    id : 2468,
+    username: "yow",
+    email: "vanz@gmail.com",
+    password:"vince",
+    recoveryKey:"art"
+  })
+
+
+  const add = async (e:SyntheticEvent) =>{
+    e.preventDefault()
+   Add_Sample(sample).then(res => {
+    console.log("Hello: ",res)
+   }).catch(err => {
+    console.log("err: ",err)
+   })
+    // 
+  }
   return (
-    <div>Login_Page</div>
+    <button onClick={add}>Login_Page</button>
   )
 }
 
