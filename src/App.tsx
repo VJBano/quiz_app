1382ba408e9752a0b1d5eac4f2b45ff40e42b033
vincent <vincentjaybano45@gmail.com>
2023-06-23 10:17:37 +0800

    add: message

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-
-import Home_page from './pages/home_page'
-import Add_quiz from './pages/add_quiz';
-import Multiple_Choice from './components/multiple_choice';
 import Router from './routes/Router';
 import { ScreenDimensionProps } from './models/screen_types';
 import { debounce } from 'lodash';
@@ -39,7 +35,7 @@ function App() {
   
   return (
     <div className="App ">
-      {mobile? <h1>Download the App </h1>: <Router/>}
+      {mobile? <h1>Download the App. coming soon... </h1>: <Router/>}
     
     </div> 
   )
