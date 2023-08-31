15d4279b4009f51a695fc4910c6c4c8a31ec81f4
vincent <vincentjaybano45@gmail.com>
2023-08-31 13:37:44 +0800

    note:[unde maintenance]

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,16 @@
 import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import Router from './routes/Router';
-import { ScreenDimensionProps } from './models/screen_types';
 import { debounce } from 'lodash';
-import { isMobileDevice } from './services/detect_mobile';
+import { isMobileDevice } from './web/services/detect_mobile';
 
 function App() {
 
   const [mobile, setMobile] = useState(false)
-
-  const [screenSize, setScreenSize] = useState<ScreenDimensionProps>({width:window.innerWidth, height:window.innerHeight})
-
-  useEffect(() => {
-
-    const handleResize = debounce(() => {
-
-      setScreenSize({height:window.innerHeight, width:window.innerWidth})
-    },100)
-
-    window.addEventListener('resize', handleResize);
-
-    return () => {
-      window.removeEventListener('resize', handleResize);
-    };
-  },[])
-
-  useEffect(() => {
-
-    setMobile(isMobileDevice())
-
-  },[screenSize])
   
   return (
     <div className="App ">
-      {mobile? <h1>Download the App. coming soon... </h1>: <Router/>}
+     Project under maintenance...
     </div> 
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import Auth_Prodiver from "./context/auth_provider";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Auth_Prodiver>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </Auth_Prodiver>
   </React.StrictMode>
 );
